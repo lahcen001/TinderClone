dceba1bf195b57c0ee01d857c5a065e6a8c42613
lahcen001e <lahcen0000lh@gmail.com>
2021-04-27 01:04:06 +0000

    v8

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+import Constants from 'expo-constants';
+import TopBar from './components/TopBar'
+import axios from 'axios';
+import SwipeableImage from './components/SwipeableImage';
+import BottomBar from './components/BottomBar';
+import Swipes from './components/Swipes';
 
 export default function App() {
+  const [users, setUsers] = useState([])
+  const [currentIndex, setCurrentIndex] = useState(0)
+  async function fetchUsers() {
+    try {
+      const { data } = await axios.get('https://randomuser.me/api/?gender=male&results=10')
+      setUsers(data.results)
+      console.log(data.results)
+    } catch (error) {
+      console.log(error)
+      Alert.alert('Error getting usres', '', [{ text: 'Retry', onPress: () => fetchUsers() }])
+    }
+  }
+  useEffect(() => {
+    fetchUsers()
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TopBar />
+      <View style={styles.swipes}>
+        {users.length > 1 && (
+          <Swipes currentIndex={currentIndex} users={users} />
+        )
+
+        }
+      </View>
+      <BottomBar />
     </View>
   );
 }
@@ -14,8 +43,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Constants.statusBarHeight
   },
+  swipes: {
+    flex: 1,
+    padding: 10,
+    paddingTop: 8,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.29,
+    shadowRadius: 4.66,
+    elevation: 7,
+  }
 });
---
+++ components/SwipeableImage.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { View, Image, StyleSheet, Text } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons'
+
+export default function SwipeableImage({ user }) {
+    console.log(user.picture.large)
+    return (
+
+        <View>
+            <Image source={{ uri: user.picture.large }} style={styles.photo} />
+            <View style={styles.textContainer}>
+                <View style={styles.textRow}>
+                    <Text style={[styles.textPrimary, styles.textShadow]}>{user.name.first}</Text>
+                    <Text style={[styles.textSecondary, styles.textShadow]} >{user.dob.age}</Text>
+                </View>
+                <View style={styles.textRow}>
+                    <FontAwesome name="map-marker" size={20} color="white" />
+                    <Text style={[styles.textSecondary, styles.textShadow]} >{user.location.city}</Text>
+                </View>
+            </View>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    photo: {
+        height: 500,
+        resizeMode: 'cover',
+        borderRadius: 20,
+    },
+    textContainer: {
+        position: 'absolute',
+        bottom: 20,
+        left: 20,
+    },
+    textPrimary: {
+        color: 'white',
+        fontSize: 25,
+        fontWeight: 'bold',
+
+    },
+    textRow: {
+        flexDirection: 'row',
+        alignItems: 'center'
+
+
+    },
+    textSecondary: {
+        color: 'white',
+        fontSize: 25,
+        marginLeft: 10,
+    },
+    textShadow: {
+        textShadowColor: 'rgba(0,0,0,0.80)',
+        textShadowOffset: { width: -1, height: 1 },
+        textShadowRadius: 10,
+    }
+
+})
